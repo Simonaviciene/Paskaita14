4910854c931e9ef40e3764476f16945b6cb275df
Vilija <Simonaviciene@users.noreply.github.com>
2020-02-15 09:54:40 +0200

    2

---
+++ Lesson11_01.IAC/ClassLibrary1/ReportGenerator.cs
@@ -0,0 +1,27 @@
+﻿using IAC.BL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IAC.BL
+{
+    class ReportGenerator
+    {
+        private AircraftRepository aircraftRepository;
+        private AircraftModelRepository aircraftModelRepository;
+        private CompanyRepository companyRepository;
+        private CountryRepository countryRepository;
+
+        public ReportGenerator(AircraftRepository aircraftRepository, AircraftModelRepository aircraftModelRepository, CompanyRepository companyRepository, CountryRepository countryRepository)
+        {
+            this.aircraftRepository = aircraftRepository;
+            this.aircraftModelRepository = aircraftModelRepository;
+            this.companyRepository = companyRepository;
+            this.countryRepository = countryRepository;
+        }
+       // public List<ReportItem> GenerateReportAircraftInEurope()
+       // {
+           // List<ReportItem> 
+       // }
+    }
+}
